ec85a955edd184c26ba43ac0cecbff98001c2745
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-25 19:57:46 +0500

    AI Batch 01

---
+++ Module 02/JavaScript/Variables.js
@@ -0,0 +1,43 @@
+// variables -- js
+//  how to print in js
+
+// containers - an element to store anything
+
+// There are 3 ways to define a variable in Js
+
+
+let a = "";
+
+// let 1223 = 
+// let 12a =
+// let Symbol() = "";
+
+
+// let a1 = "Hammad"; 
+// let a2 = "Anas"; 
+// let a3 = "Munsif"; 
+
+
+
+// string = "Shaheer"
+// string = "10"
+// string = "True/False"
+
+
+// integer = 1-100
+
+//  Boolean = True or False
+
+
+// const a = "Shaheer"
+
+// How to declare a variable
+let x = 75; 
+let y = "Hammad Anas Munsif"; 
+let z = false;
+
+console.log("Karachi & Pakistan")
+
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof z)
---
+++ Module 02/JavaScript/script.js
@@ -0,0 +1,19 @@
+// console.log("My name is Shaheer")
+
+
+// Variables in JavaScript
+
+// Data Types in Java Script
+
+// String = 'Shaheer' or "Shaheer"
+// Integer = 100  45 
+// Float = 10.98 48.12
+// Boolean = True or Flase
+
+let x = "Karachi"
+let y = "Pakistan"
+
+
+// Karachi is the city of Pakistan
+
+console.log(x, " is the city of ", y)
