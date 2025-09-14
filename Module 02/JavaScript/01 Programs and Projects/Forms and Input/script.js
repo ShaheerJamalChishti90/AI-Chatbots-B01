b71fad3f0b990befe9572ea6ec27c1ed3bd1dd94
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-09-14 15:17:04 +0500

    JavaScript 2025

---
+++ Module 02/JavaScript/01 Programs and Projects/Forms and Input/script.js
@@ -0,0 +1,17 @@
+const forms = document.getElementById("userForm")
+
+forms.addEventListener("forms", )
+
+
+function event(event){
+    // Auto refresh nahi hoga
+    event.preventDefault(); 
+
+    let name = document.getElementById("name") 
+    let age = document.getElementById("age")
+    let email = document.getElementById("email")
+
+    
+
+}
+
