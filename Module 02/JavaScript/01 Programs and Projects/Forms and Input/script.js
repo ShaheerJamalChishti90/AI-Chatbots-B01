086d8a4e893a08025d13422000e962c5357756a7
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-09-14 15:56:02 +0500

    JavaScript 2025

--- Module 02/JavaScript/01 Programs and Projects/Forms and Input/script.js
+++ Module 02/JavaScript/01 Programs and Projects/Forms and Input/script.js
@@ -1,17 +1,23 @@
-const forms = document.getElementById("userForm")
+const forms = document.getElementById("userForm");
 
-forms.addEventListener("forms", )
+forms.addEventListener("submit", myforms)
 
 
-function event(event){
+function myforms(){
     // Auto refresh nahi hoga
-    event.preventDefault(); 
+    // event.preventDefault(); 
 
-    let name = document.getElementById("name") 
-    let age = document.getElementById("age")
-    let email = document.getElementById("email")
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let email = document.getElementById("email").value;
 
-    
+    let content = `Name: ${name}\nAge: ${age}\n Email: ${email}`;
 
-}
+    let blob = new Blob([content], {type: "text/plain"});
+
+    let download_data = document.createElement("a");
+    download_data.href = URL.createObjectURL(blob);
+    download_data.download = `${name}.txt`;
+    download_data.click();
+};
 
