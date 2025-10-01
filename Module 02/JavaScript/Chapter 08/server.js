9c9efc3706a6b47b6bfeecd1a57cd25eead06627
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-10-01 19:12:30 +0500

    Useful Material

--- Module 02/JavaScript/Chapter 08/server.js
+++ Module 02/JavaScript/Chapter 08/server.js
@@ -1,5 +1,28 @@
 // Server: Whats a server?
 // CURD -> Creat | Update | Read | Delete 
+// npm install express (to install the express js)
+// we will initialize it (npm init)
+// npm init -y
+
+// Import express
+const express = require("express");
+
+// Create an express app
+const app = express();
+
+// Define a port
+const PORT = 3000;
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello, Shaheer Jamal is teaching");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
 
 
 
